802933673c8b6bf1f54f6482025e5e5c5cd98c9e
michael.roy-diclemen <michael.roy-diclemen@TEACH2D-C3NYBS3.EDU.YRDSB.NET>
2024-10-10 11:31:08 -0400

    Initial Commit

---
+++ src/rdmp/RDMP.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package rdmp;
+
+/**
+ *
+ * @author michael.roy-diclemen
+ */
+public class RDMP {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //INPUT
+        
+        //PASSWORD CHECK
+        
+        //IDENTIFY GROUP
+        
+        //Menu
+            //butoon presses
+            
+        //Output Result
+        
+        
+    }
+    
+}
